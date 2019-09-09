41df0b1f567c4611549c7b99237160cc8622d427
Andrew Mocol <AndrewMocol@hotmail.com>
2019-09-08 19:40:07 -0500

    Added Material buttons and some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { OpenSourceComponent } from './views/open-source/open-source.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { A11yModule } from '@angular/cdk/a11y/typings'
 
 @NgModule({
   declarations: [
@@ -26,7 +30,11 @@ import { PageNotFoundComponent } from './views/page-not-found/page-not-found.com
     BrowserModule,
     AppRoutingModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    A11yModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
