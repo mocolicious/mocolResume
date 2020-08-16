03ff9dedec337a8b2712e0a2049c4082fec7adaf
Andrew <andrewmocol@hotmail.com>
2020-08-15 22:27:22 -0500

    update

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,7 +1,10 @@
-import {NgModule} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLinkModule, HttpLink} from 'apollo-angular/http';
+import {InMemoryCache} from '@apollo/client/core';
+import {NgModule} from '@angular/core';
+
+
+
 
 const uri = 'https://api.github.com/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
