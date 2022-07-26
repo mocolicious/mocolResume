861928e2483badfb8b5496c2d6f6f3c81b4bfc60
Andrew <andrewmocol@hotmail.com>
2022-07-25 22:22:34 -0500

    update angular to 11

--- src/app/components/busy/busy.component.spec.ts
+++ src/app/components/busy/busy.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BusyComponent } from './busy.component';
 
@@ -6,7 +6,7 @@ describe('BusyComponent', () => {
   let component: BusyComponent;
   let fixture: ComponentFixture<BusyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BusyComponent ]
     })
