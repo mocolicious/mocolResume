5c6d0730a8f9aca4983cc898686fd2f21080db71
Andrew <andrewmocol@hotmail.com>
2023-11-26 22:07:05 -0600

    made servicable layout

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core'; 
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [MatGridListModule],
 })
 export class HomeComponent implements OnInit {
 
