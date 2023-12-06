1c6c7329a0bc3bf03fa77e5f8a0ea0a2e95b9c3f
Andrew <andrewmocol@hotmail.com>
2023-12-05 23:44:23 -0600

    Update home.component.ts

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core'; 
 import {MatGridListModule} from '@angular/material/grid-list';
+import { RouterLinm } from '@angular/router'
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, RouterLink],
 })
 export class HomeComponent implements OnInit {
 
